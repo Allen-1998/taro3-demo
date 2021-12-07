ab4962b4e926cfc8808045f0a4e30a5c0feff145
gzh <272970666@qq.com>
2021-12-07 10:05:29 +0800

    feat: pre-commit优化

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
-import { createApp } from "vue";
-import { Button, Toast } from "@nutui/nutui-taro";
+import { createApp } from 'vue'
+import { Button, Toast } from '@nutui/nutui-taro'
 // import "@nutui/nutui-taro/dist/style.css";
-import "@nutui/nutui-taro/dist/styles/themes/default.scss";
+import '@nutui/nutui-taro/dist/styles/themes/default.scss'
 
-import "./app.scss";
+import './app.scss'
 
 const App = createApp({
-  onShow(options) {},
+  onShow(options) {
+    console.log(options)
+  },
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
-});
+})
 
-App.use(Button).use(Toast);
+App.use(Button).use(Toast)
 
-export default App;
+export default App
